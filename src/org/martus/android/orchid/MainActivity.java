07a0ab43be87d9e4b30055b96d073e23dd33b687
Rom Srinivasan <roms@benetech.org>
2013-07-05 13:49:53 -0700

    Testing connection to server with Tor

--- src/org/martus/android/orchid/MainActivity.java
+++ src/org/martus/android/orchid/MainActivity.java
@@ -1,63 +1,112 @@
 package org.martus.android.orchid;
 
-import java.io.File;
+import java.net.URL;
+
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import com.subgraph.orchid.TorClient;
 import com.subgraph.orchid.TorInitializationListener;
+import com.subgraph.orchid.xmlrpc.OrchidXmlRpcTransportFactory;
 
 public class MainActivity extends Activity
 {
 
 	private TorClient tor;
 	private static final String APP_LABEL = "orchid-android";
+	private TextView screenText;
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+	    screenText = (TextView)findViewById(R.id.mainText);
 
 	    createRealTorClient();
-	    tor.start();
     }
 
 	private void createRealTorClient()
 	{
 		tor = new TorClient();
-		//File torDirectory = new File(this.getCacheDir().getParent(), "shared_prefs");
-        //torDirectory.mkdirs();
-		//tor.getConfig().setDataDirectory(this.getExternalFilesDir(null));
+
 		tor.getConfig().setDataDirectory(this.getCacheDir());
+		final AsyncTask<TorClient, Object, String> initializeTask = new InitializeTask();
+		Log.i(APP_LABEL, "about to start initialization");
+		initializeTask.execute(tor);
+	}
 
+	private void onInitialized()  {
+		screenText.setText("Tor Initialized");
+		testTor();
+	}
 
-		class TorInitializationHandler implements TorInitializationListener
-		{
-			public void initializationProgress(String message, int percent)
-			{
-				updateProgress(message, percent);
-			}
+	private void testTor() {
+		try {
+			XmlRpcClient client = new XmlRpcClient();
+			XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+			client.setConfig(config);
 
-			public void initializationCompleted()
-			{
-				updateProgressComplete();
-			}
+			// Note: There is also a three argument constructor available which allows passing an SSLContext instance
+			//       that you have previously configured with a customized trust manager.
+			//
+			//    JTorXmlRpcTransportFactory(XmlRpcClient client, TorClient torClient, SSLContext sslContext)
 
+			client.setTransportFactory(new OrchidXmlRpcTransportFactory(client,tor));
+
+			// Just some random xmlrpc service on the internet that accepts unauthenticated requests
+			config.setServerURL(new URL("http://xmlrpc.texy.info/"));
+			Log.i(APP_LABEL, "Result from texy1.version is "+ client.execute("texy1.version", new Object[0]));
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-
-		tor.addInitializationListener(new TorInitializationHandler());
 	}
 
-	private void updateProgress(String message, int percent)
+	private class InitializeTask extends AsyncTask<TorClient, Object, String> implements  TorInitializationListener
 	{
-		Log.i(APP_LABEL, "Tor initialization: " + percent + "% - " + message);
-	}
+        @Override
+        protected String doInBackground(TorClient... clients) {
 
-	private void updateProgressComplete()
-	{
-		Log.i(APP_LABEL,"Tor initialization complete");
-	}
+            final TorClient client = clients[0];
+
+	        client.addInitializationListener(this);
+	        tor.start();
+            tor.enableSocksListener();
+	        try
+	        {
+		        tor.waitUntilReady();
+	        } catch (InterruptedException e)
+	        {
+		        Log.e(APP_LABEL, "error", e);
+	        }
+	        return "Tor Initialized";
+        }
+
+		public void initializationProgress(String message, int percent)
+        {
+            onProgressUpdate(percent, message);
+        }
+
+        public void initializationCompleted()
+        {
+            onProgressUpdate(101, "Tor initialization complete");
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+	        onInitialized();
+        }
+
+		@Override
+	    protected void onProgressUpdate(Object... progress) {
+	        super.onProgressUpdate(progress);
+			Log.i(APP_LABEL, "Tor initialization: " + progress[0] + "% - " + progress[1]);
+	    }
+    }
 }
